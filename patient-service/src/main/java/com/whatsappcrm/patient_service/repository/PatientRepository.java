e8600b28508c6ef51b72243241f947aa19581d96
Ayush Gupta <ayushgupta028@gmail.com>
2026-08-16 17:56:54 +0530

    test(patient): add patient integration tests

--- patient-service/src/main/java/com/whatsappcrm/patient_service/repository/PatientRepository.java
+++ patient-service/src/main/java/com/whatsappcrm/patient_service/repository/PatientRepository.java
@@ -4,6 +4,8 @@ import com.whatsappcrm.patient_service.entity.Patient;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -19,5 +21,10 @@ public interface PatientRepository extends JpaRepository<Patient,Long> {
             String mobileNumber,
             Pageable pageable
     );
+    @Query(
+            value = "SELECT * FROM patients WHERE id = :id",
+            nativeQuery = true
+    )
+    Optional<Patient> findByIdIncludingDeleted(@Param("id") Long id);
 
 }
