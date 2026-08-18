d10c07f017e0fa9cf52a27f5c94445210b918cac
Ayush Gupta <ayushgupta028@gmail.com>
2026-08-18 15:58:38 +0530

    feat: tenant aware patient service and integration test changes

--- patient-service/src/main/java/com/whatsappcrm/patient_service/repository/PatientRepository.java
+++ patient-service/src/main/java/com/whatsappcrm/patient_service/repository/PatientRepository.java
@@ -11,16 +11,47 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 @Repository
 public interface PatientRepository extends JpaRepository<Patient,Long> {
-    Optional<Patient> findByMobileNumber(String mobileNumber);
 
-    boolean existsByMobileNumber(String mobileNumber);
+    Optional<Patient> findByTenantIdAndId(
+            Long tenantId,
+            Long id
+    );
+    Optional<Patient> findByTenantIdAndMobileNumber(
+            Long tenantId,
+            String mobileNumber
+    );
 
-    Page<Patient> findByFirstNameContainingIgnoreCaseOrLastNameContainingIgnoreCaseOrMobileNumberContaining(
+    boolean existsByTenantIdAndMobileNumber(
+            Long tenantId,
+            String mobileNumber
+    );
+
+    Page<Patient> findByTenantIdAndFirstNameContainingIgnoreCaseOrTenantIdAndLastNameContainingIgnoreCaseOrTenantIdAndMobileNumberContaining(
+            Long tenantId1,
             String firstName,
+            Long tenantId2,
             String lastName,
+            Long tenantId3,
             String mobileNumber,
             Pageable pageable
     );
+    @Query("""
+    SELECT p
+    FROM Patient p
+    WHERE p.tenantId = :tenantId
+      AND (
+           LOWER(p.firstName) LIKE LOWER(CONCAT('%', :keyword, '%'))
+        OR LOWER(p.lastName) LIKE LOWER(CONCAT('%', :keyword, '%'))
+        OR p.mobileNumber LIKE CONCAT('%', :keyword, '%')
+      )
+""")
+    Page<Patient> searchPatients(
+            @Param("tenantId") Long tenantId,
+            @Param("keyword") String keyword,
+            Pageable pageable
+    );
+
+    Page<Patient> findAllByTenantId(Long tenantId,Pageable pageable);
     @Query(
             value = "SELECT * FROM patients WHERE id = :id",
             nativeQuery = true
